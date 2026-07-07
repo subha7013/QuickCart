7dbf1b8da9b76d1ecd804415af67848ec1b449ee
Subhasish Nath <nathsubhasish55@gmail.com>
2026-07-07 12:44:34 +0530

    Update App.jsx (Backend Link updated)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useMemo, useState } from 'react';
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://quickcart-39cz.onrender.com';
 
 const categoryMap = {
     'Electronics': ['Mobile', 'Headset', 'Accessories', 'Speakers'],
