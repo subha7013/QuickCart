feb003af2859fa05b05ca345e33ddce972bfeddc
Subhasish Nath <nathsubhasish55@gmail.com>
2026-07-08 12:23:47 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,11 +22,7 @@ app.use(cors({
   origin: (origin, callback) => {
     const allowedOrigins = [
       "https://orderquickcart.netlify.app",
-      "https://speedmart-order.netlify.app",
-      "http://127.0.0.1:5500",
-      "http://localhost:5173",
-      "http://localhost:3000",
-      "http://localhost:3001"
+      "http://localhost:3000"
     ];
 
     if (!origin || allowedOrigins.includes(origin)) {
